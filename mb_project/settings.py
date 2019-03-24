151bb77950df3d89b47282bcc1c94082de51e170
Paul Barajas <pbarajas@contractroom.com>
2019-03-24 17:42:14 -0600

    adding allowed host true

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9*9ig6y&)^qedf1=a0za=akj6^q%*^!njsk#hodw=aw$%dut!d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
